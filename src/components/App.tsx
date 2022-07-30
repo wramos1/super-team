e080a270e2efc90a61ecb086a268d237d02e127d
Willyam Ramos <ramoswillyam1@gmail.com>
2022-07-29 21:34:47 -0700

    Setup BrowserRouter and Routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import RouterURLs from './RouterURLs';
 
 const App = () => {
