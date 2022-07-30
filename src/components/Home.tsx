8faf5c240268f1f0455091a351cb19da86433d38
Willyam Ramos <ramoswillyam1@gmail.com>
2022-07-29 21:40:05 -0700

    Began first tests of API calls data into state

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,63 @@
 import React from 'react'
+import { useState } from 'react';
+import supe from '../apis/supe'
+import SuperPerson from '../models/superPerson';
 
 const Home = () => {
+    const [team, setTeam] = useState<SuperPerson[]>([]);
+
+
+    const getSpidey = supe.get('', {
+        params: {
+            hero: 'Spiderman'
+        }
+    });
+
+    const getDeadpool = supe.get('', {
+        params: {
+            hero: 'Deadpool'
+        }
+    })
+
+    const getFlash = supe.get('', {
+        params: {
+            hero: 'Flash'
+        }
+    })
+
+    const getScarletWitch = supe.get('', {
+        params: {
+            hero: 'Scarlet Witch'
+        }
+    })
+    const getSilverSurfer = supe.get('', {
+        params: {
+            hero: 'Silver Surfer'
+        }
+    })
+
+    const superAPIList = [getSpidey, getDeadpool, getFlash, getScarletWitch, getSilverSurfer]
+
+
+    const handleClick = async (): Promise<void> => {
+        const res = await Promise.all(superAPIList.map(async (item) => {
+            return (await item).data
+        }));
+
+        setTeam(res);
+    };
+
+    function check() {
+        console.log(team)
+    }
+
     return (
-        <div>Home</div>
+        <div>
+            <button onClick={handleClick}>
+                Click
+            </button>
+            <button onClick={check}>Check</button>
+        </div>
     )
 }
 
