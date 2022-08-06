af1fa88dfb0755a58791d8acad78f611b54f5167
Willyam Ramos <ramoswillyam1@gmail.com>
2022-08-05 22:20:54 -0700

    Revised Router URLs to add new components and clarify existing components paths

--- src/components/RouterURLs.tsx
+++ src/components/RouterURLs.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import TheSuperTeam from './TheSuperTeam';
+import Randomize from './Randomize';
 
 const RouterURLs = () => {
     return (
         <Routes>
             <Route path={process.env.PUBLIC_URL} element={<Home />} />
+            <Route path={'/the-super-team'} element={<TheSuperTeam />} />
+            <Route path={'/randomize-team'} element={<Randomize />} />
         </Routes>
     )
 }
