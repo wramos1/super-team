07482d8ca0f57017806c1a23bf6bc285c9eac99b
Willyam Ramos <ramoswillyam1@gmail.com>
2022-08-05 22:20:02 -0700

    Designed and made home page component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,64 +1,35 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import supe from '../apis/supe'
-import SuperPerson from '../models/superPerson';
-import TheSuperTeam from './TheSuperTeam';
+import React from 'react';
+import '../styles/Home.css';
+import supers from '../supers.png';
 
 const Home = () => {
-    //UseState to contain data pertaining to My Team
-    const [team, setTeam] = useState<SuperPerson[]>([]);
-
-    //UseEffect renders the data from API calls on initial render
-    useEffect(() => {
-        fetchSuperTeam();
-    }, [])
-
-    //API calls with different Params
-    const getSpidey = supe.get('', {
-        params: {
-            hero: 'Spiderman'
-        }
-    });
-
-    const getDeadpool = supe.get('', {
-        params: {
-            hero: 'Deadpool'
-        }
-    })
-
-    const getFlash = supe.get('', {
-        params: {
-            hero: 'Flash'
-        }
-    })
-
-    const getScarletWitch = supe.get('', {
-        params: {
-            hero: 'Scarlet Witch'
-        }
-    })
-    const getSilverSurfer = supe.get('', {
-        params: {
-            hero: 'Silver Surfer'
-        }
-    })
-
-    //Array of API calls to simplify async function below
-    const superAPIList = [getSpidey, getDeadpool, getFlash, getScarletWitch, getSilverSurfer];
-
-
-    //Function to combine data from all API calls and store them in UseState team
-    const fetchSuperTeam = async (): Promise<void> => {
-        const res = await Promise.all(superAPIList.map(async (item) => {
-            return (await item).data
-        }));
-
-        setTeam(res);
-    };
-
     return (
-        <div>
-            <TheSuperTeam team={team} />
+        <div id='home'>
+            <h1>
+                Welcome
+            </h1>
+
+            <h2>
+                To the home of <span>SUPER-TEAMS</span>
+            </h2>
+
+            <img src={supers} alt="superheroes" />
+
+            <div id='tabDescriptors'>
+                <h4>
+                    <span>Randomize</span> a Super-Team
+                </h4>
+
+                <h4>
+                    <span>Build</span> your own Super-Team
+                </h4>
+
+                <h4>
+                    <span>View my (THE)</span> Super-Team
+                </h4>
+            </div>
+
+            <h6>Does your super-team beat mine?</h6>
         </div>
     )
 }
