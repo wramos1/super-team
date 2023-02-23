f101cc5c1ad21e6b52d76d1a0f2a4c300202ba0f
Willyam Ramos <ramoswillyam1@gmail.com>
2023-02-22 17:27:36 -0800

    Fixed styling issues on home, responsive navbar, and home buttons. Allowed home buttons to navigate to different pages after relating each one to its appropriate navLink in order to still update the navbar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Home.css';
 import supers from '../supers.png';
 
 const Home = () => {
+
+    useEffect(() => {
+        tabSelecting();
+    }, [])
+
+
+    const tabSelecting = () => {
+        const tabs = document.getElementsByClassName('home-nav-link');
+        const navTabs = document.getElementsByClassName('nav-link');
+
+        for (let i = 0; i < tabs.length; i++) {
+
+            tabs[i].addEventListener('click', () => {
+                const previousSelects = document.getElementsByClassName('active');
+                if (previousSelects.length > 0) {
+                    previousSelects[0].className = previousSelects[0].className.replace('active', '');
+                }
+                if (i === 0) {
+                    navTabs[2].classList.add('active');
+                }
+                else if (i === 1) {
+                    navTabs[3].classList.add('active');
+                }
+                else if (i === 2) {
+                    navTabs[1].classList.add('active');
+                }
+            })
+        };
+    };
+
     return (
         <div id='home'>
             <h1>
@@ -16,17 +47,17 @@ const Home = () => {
             <img src={supers} alt="superheroes" />
 
             <div id='tabDescriptors'>
-                <h4>
-                    <span>Randomize</span> a Super-Team
-                </h4>
+                <Link to={'/randomize-team'} className="home-nav-link" >
+                    Randomize a Super-Team
+                </Link>
 
-                <h4>
-                    <span>Build</span> your own Super-Team
-                </h4>
+                <Link to={'/build-team'} className="home-nav-link">
+                    Build your own Super-Team
+                </Link>
 
-                <h4>
-                    <span>View my (THE)</span> Super-Team
-                </h4>
+                <Link to={'/the-super-team'} className="home-nav-link">
+                    View my (THE) Super-Team
+                </Link>
             </div>
 
             <h6>Does your super-team beat mine?</h6>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import '../styles/Navbar.css'
 
 const Navbar = () => {
+
+    const location = useLocation();
 
     const [tab, setTab] = useState<string>('Home');
 
     useEffect(() => {
         tabSelecting();
+        console.log('useeffect ran')
     }, [])
 
     const tabSelecting = () => {
